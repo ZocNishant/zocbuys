556c7a8f9761622afcdbfba2b4014fd29b38d0e3
Zoc Nishant <zocnishant@gmail.com>
2024-09-03 07:47:16 +0545

    products screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -11,7 +11,7 @@ const HomeScreen = () => {
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <h3>
-              <Product product={product} />?
+              <Product product={product} />
             </h3>
           </Col>
         ))}
